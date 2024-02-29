d975c7df8e07603c9cc067f64910b00d03d8b649
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2024-02-29 13:11:40 +0900

    allow 4x-Ultrasharp

--- auto1111sdk/modules/shared_options.py
+++ auto1111sdk/modules/shared_options.py
@@ -94,7 +94,7 @@ options_templates.update(options_section(('saving-to-dirs', "Saving to a directo
 options_templates.update(options_section(('upscaling', "Upscaling", "postprocessing"), {
     "ESRGAN_tile": OptionInfo(192, "Tile size for ESRGAN upscalers.", {"minimum": 0, "maximum": 512, "step": 16}).info("0 = no tiling"),
     "ESRGAN_tile_overlap": OptionInfo(8, "Tile overlap for ESRGAN upscalers.", {"minimum": 0, "maximum": 48, "step": 1}).info("Low values = visible seam"),
-    "realesrgan_enabled_models": OptionInfo(["R-ESRGAN 4x+", "R-ESRGAN 4x+ Anime6B"], "Select which Real-ESRGAN models to show in the web UI.", lambda: {"choices": shared_items.realesrgan_models_names()}),
+    "realesrgan_enabled_models": OptionInfo(["R-ESRGAN 4x+", "R-ESRGAN 4x+ Anime6B", "4x-Ultrasharp"], "Select which Real-ESRGAN models to show in the web UI.", lambda: {"choices": shared_items.realesrgan_models_names()}),
     "upscaler_for_img2img": OptionInfo(None, "Upscaler for img2img", lambda: {"choices": [x.name for x in shared.sd_upscalers]}),
 }))
 
