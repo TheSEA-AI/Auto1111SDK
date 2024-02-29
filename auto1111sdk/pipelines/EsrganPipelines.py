e8b17263e28fbda8816a4f3da270c2cf72476a2b
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2024-02-29 15:02:53 +0900

    ultrasharp

--- auto1111sdk/pipelines/EsrganPipelines.py
+++ auto1111sdk/pipelines/EsrganPipelines.py
@@ -54,7 +54,7 @@ real_esrgan_info = {
     "R-ESRGAN 4x+": {
         "scale": 4, 
         "model": lambda: RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4), 
-        "path":"https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth"
+        "path":"https://huggingface.co/datasets/Kizi-Art/Upscale/blob/fa98e357882a23b8e7928957a39462fbfaee1af5/4x-UltraSharp.pth"
     }, 
     "4x-UltraSharp": {
         "scale": 4, 
